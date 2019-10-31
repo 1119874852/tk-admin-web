99a7b1fa588a5b80822a0b8ae0dc2fade6639248
liujiaxin <1119874852@qq.com>
2019-10-31 14:22:42 +0800

    上传分布式管理系统前端

---
+++ src/views/introduction/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="app-container">
+  <div class="wrapper">
+   
+  </div>
+</div>
+</template>
+
+<style scoped>
+.wrapper {
+  width: 800px;
+  margin: 30px auto;
+}
+</style>
